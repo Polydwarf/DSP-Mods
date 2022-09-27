bd013da077557a336a6994418a950b3f93c7402f
Jason Frey <jasonf@shotzoom.com>
2022-09-27 07:26:25 -0700

    Adding post build call to copy dll over to thunderstore dir (bat file intentionally not checked in because... no one needs to know my paths... but it's a simple one line bat file to copy the dll over) tweaking guid and whatnot to not step on original

--- DistributeSpaceWarper/Plugin.cs
+++ DistributeSpaceWarper/Plugin.cs
@@ -4,26 +4,27 @@ using HarmonyLib;
 
 namespace DistributeSpaceWarper
 {
-    [BepInPlugin( PluginGuid, PluginName, PluginVersion)]
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
     [BepInProcess("DSPGAME.exe")]
     public class Plugin : BaseUnityPlugin
     {
-        private static ManualLogSource _logger;
-        private const string PluginGuid = "ShadowAngel.DSP.DistributeSpaceWarper";
-        private const string PluginName = "Distribute Space Warper";
-        private const string PluginVersion = "1.0.8.0";
+        public static ManualLogSource _logger;
+        private const string PluginGuid = "PD820.DSP.DistributeSpaceWarper.Remix";
+        private const string PluginName = "Distribute Space Warper Remix";
+        private const string PluginVersion = "1.0.0.0";
 
         public void Awake()
         {
+            _logger = base.Logger;
             Harmony harmony = new Harmony(PluginGuid);
             DistributeSpaceWarper.Config.Init(Config);
             harmony.PatchAll(typeof(Plugin));
             harmony.PatchAll(typeof(Patch));
+            _logger.LogMessage("Awaken the Remixed distributed space warper");
         }
         public void Start()
         {
-            _logger = base.Logger;
-            _logger.LogInfo("Loaded!");
+            _logger.LogMessage("Start the Remixed distributed space warper");
         }
     }
 }
